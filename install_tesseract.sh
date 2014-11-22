797edd2325ab46af120b8cb8f0103c36c3a758f9
Stephen Margheim <margheim@sas.upenn.edu>
2014-11-21 20:46:28 -0500

    Create install_tesseract.sh

---
+++ install_tesseract.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# courtesy of : <https://ryanfb.github.io/etc/2014/11/13/command_line_ocr_on_mac_os_x.html>
+
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+# Ensure `homebrew` is up-to-date and ready
+echo "Updating homebrew..."
+brew doctor
+
+# Ensure the Homebrew cache directory exists
+mkdir ~/Library/Caches/Homebrew
+
+# Install leptonica with TIFF support (and every other format, just in case)
+echo "Installing leptonica..."
+brew install --with-libtiff --with-openjpeg --with-giflib leptonica
+
+# Install Ghostscript
+echo "Installing ghostscript..."
+brew install gs
+
+# Install ImageMagick with TIFF and Ghostscript support
+echo "Installing imagemagick..."
+brew install --with-libtiff --with-ghostscript imagemagick
+
+# Install Tesseract devel with all languages
+echo "Installing tesseract..."
+brew install --devel --all-languages tesseract
